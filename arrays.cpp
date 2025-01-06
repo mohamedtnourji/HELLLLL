e203d51c38491212ed15f9bae8625e2e39765f7d
mohamed <mohamed@tnourji50>
2025-01-06 13:10:46 +0100

    .

---
+++ arrays.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    int arr[3][2];
+    for (size_t i = 0; i < 3; i++)
+    {
+        for (size_t j = 0; j< 2; j++)
+        {
+            cout << "enter mark of student #" << i+1 << ": " << endl;
+            cin >> arr[i][j];
+        }
+        
+    }
+    double sum = 0;
+    double avg[3];
+    for (size_t i = 0; i < 3; i++)
+    {
+        sum = 0;
+        for (size_t j = 0; j < 2; j++)
+        {
+            sum += arr[i][j];
+        }
+        avg[i] = sum/5;
+        cout << "the average of student #" << i+1 << "is: " << avg[i] << endl;
+    }
+    
+    return 0;
+}
